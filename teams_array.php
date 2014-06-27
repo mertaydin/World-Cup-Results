174936d8204da845e168eb9862251b0fc8503110
Mert Aydın <mert_aydin5@hotmail.com>
2014-06-27 21:24:24 +0300

    teams_array.php added to get the real name of country that I have fifa code

---
+++ teams_array.php
@@ -0,0 +1,260 @@
+<?php
+
+$teams_array = array (
+    0 => 
+    array
+        (
+            'country' => 'Brazil',
+            'alternate_name' => '',
+            'fifa_code' => 'BRA',
+            'group_id' => '1'
+        ),
+    1 => array
+        (
+            'country' => 'England',
+            'alternate_name' => '',
+            'fifa_code' => 'ENG',
+            'group_id' => '4'
+        ),
+    2 => array
+        (
+            'country' => 'Uruguay',
+            'alternate_name' => '',
+            'fifa_code' => 'URU',
+            'group_id' => '4'
+        ),
+
+    3 => array
+        (
+            'country' => 'Italy',
+            'alternate_name' => '',
+            'fifa_code' => 'ITA',
+            'group_id' => '4'
+        ),
+
+    4 => array
+        (
+            'country' => 'Costa Rica',
+            'alternate_name' => '',
+            'fifa_code' => 'CRC',
+            'group_id' => '4'
+        ),
+
+    5 => array
+        (
+            'country' => 'Netherlands',
+            'alternate_name' => 'Holland',
+            'fifa_code' => 'NED',
+            'group_id' => '2'
+        ),
+
+    6 => array
+        (
+            'country' => 'Australia',
+            'alternate_name' => '',
+            'fifa_code' => 'AUS',
+            'group_id' => '2'
+        ),
+
+    7 => array
+        (
+            'country' => 'Spain',
+            'alternate_name' => '',
+            'fifa_code' => 'ESP',
+            'group_id' => '2'
+        ),
+
+    8 => array
+        (
+            'country' => 'Chile',
+            'alternate_name' => '',
+            'fifa_code' => 'CHI',
+            'group_id' => '2'
+        ),
+
+    9 => array
+        (
+            'country' => 'Croatia',
+            'alternate_name' => '',
+            'fifa_code' => 'CRO',
+            'group_id' => '1'
+        ),
+
+    10 => array
+        (
+            'country' => 'Cameroon',
+            'alternate_name' => '',
+            'fifa_code' => 'CMR',
+            'group_id' => '1'
+        ),
+
+    11 => array
+        (
+            'country' => 'Mexico',
+            'alternate_name' => '',
+            'fifa_code' => 'MEX',
+            'group_id' => '1'
+        ),
+
+    12 => array
+        (
+            'country' => 'Colombia',
+            'alternate_name' => '',
+            'fifa_code' => 'COL',
+            'group_id' => '3'
+        ),
+
+    13 => array
+        (
+            'country' => 'Ivory Coast',
+            'alternate_name' => 'CÃ´te d\'Ivoire',
+            'fifa_code' => 'CIV',
+            'group_id' => '3'
+        ),
+
+    14 => array
+        (
+            'country' => 'Greece',
+            'alternate_name' => '',
+            'fifa_code' => 'GRE',
+            'group_id' => '3'
+        ),
+
+    15 => array
+        (
+            'country' => 'Japan',
+            'alternate_name' => '',
+            'fifa_code' => 'JPN',
+            'group_id' => '3'
+        ),
+
+    16 => array
+        (
+            'country' => 'Iran',
+            'alternate_name' => '',
+            'fifa_code' => 'IRN',
+            'group_id' => '6'
+        ),
+
+    17 => array
+        (
+            'country' => 'Argentina',
+            'alternate_name' => '',
+            'fifa_code' => 'ARG',
+            'group_id' => '6'
+        ),
+
+    18 => array
+        (
+            'country' => 'Nigeria',
+            'alternate_name' => '',
+            'fifa_code' => 'NGA',
+            'group_id' => '6'
+        ),
+
+    19 => array
+        (
+            'country' => 'Bosnia and Herzegovina',
+            'alternate_name' => 'Bosnia-Herzegovina',
+            'fifa_code' => 'BIH',
+            'group_id' => '6'
+        ),
+
+    20 => array
+        (
+            'country' => 'France',
+            'alternate_name' => '',
+            'fifa_code' => 'FRA',
+            'group_id' => '5'
+        ),
+
+    21 => array
+        (
+            'country' => 'Switzerland',
+            'alternate_name' => '',
+            'fifa_code' => 'SUI',
+            'group_id' => '5'
+        ),
+
+    22 => array
+        (
+            'country' => 'Ecuador',
+            'alternate_name' => '',
+            'fifa_code' => 'ECU',
+            'group_id' => '5'
+        ),
+
+    23 => array
+        (
+            'country' => 'Honduras',
+            'alternate_name' => '',
+            'fifa_code' => 'HON',
+            'group_id' => '5'
+        ),
+
+    24 => array
+        (
+            'country' => 'Germany',
+            'alternate_name' => '',
+            'fifa_code' => 'GER',
+            'group_id' => '7'
+        ),
+
+    25 => array
+        (
+            'country' => 'Ghana',
+            'alternate_name' => '',
+            'fifa_code' => 'GHA',
+            'group_id' => '7'
+        ),
+
+    26 => array
+        (
+            'country' => 'Portugal',
+            'alternate_name' => '',
+            'fifa_code' => 'POR',
+            'group_id' => '7'
+        ),
+
+    27 => array
+        (
+            'country' => 'USA',
+            'alternate_name' => '',
+            'fifa_code' => 'USA',
+            'group_id' => '7'
+        ),
+
+    28 => array
+        (
+            'country' => 'Belgium',
+            'alternate_name' => '',
+            'fifa_code' => 'BEL',
+            'group_id' => '8'
+        ),
+
+    29 => array
+        (
+            'country' => 'Russia',
+            'alternate_name' => '',
+            'fifa_code' => 'RUS',
+            'group_id' => '8'
+        ),
+
+    30 => array
+        (
+            'country' => 'Algeria',
+            'alternate_name' => '',
+            'fifa_code' => 'ALG',
+            'group_id' => '8'
+        ),
+
+    31 => array
+        (
+            'country' => 'Korea Republic',
+            'alternate_name' => 'South Korea',
+            'fifa_code' => 'KOR',
+            'group_id' => '8'
+        )
+);
+
+?>
